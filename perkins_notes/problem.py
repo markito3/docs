764aeb582314bf1ae4b6021745da0ba758e93241
Mark Ito <markito3@gmail.com>
2023-05-17 21:35:46 -0400

    solved problem 1

--- perkins_notes/problem.py
+++ perkins_notes/problem.py
@@ -12,5 +12,7 @@ print(E_0, theta_degrees, m_p, E, E_0_minus_E, theta)
 print(E_0_minus_E**2, 2*E_0_minus_E*m_p, E**2*math.sin(theta)**2, (E_0 - E*math.cos(theta))**2)
 zero = E_0_minus_E**2 + 2*E_0_minus_E*m_p - E**2*math.sin(theta)**2 \
     - (E_0 - E*math.cos(theta))**2
-print(zero)
+print("zero =", zero)
+qsquared = E_0_minus_E**2-E**2*math.sin(theta)**2 - (E_0 - E*math.cos(theta))**2
+print("qsquared =", qsquared)
 exit
